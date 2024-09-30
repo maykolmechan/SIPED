e70825f53d5ca46b220c8c2d81caef9001abb078
MAYKOL MECHAN <maykol1127@gmail.com>
2024-09-30 01:47:09 -0500

    Tercer Commit corrigiendo las dos variables innecesarias en la clase Persona

--- modelos/Persona.php
+++ modelos/Persona.php
@@ -9,7 +9,7 @@ require_once 'Conexion.php';
 class Persona {
     private $db;
     //no usamos mas variables...
-    // ------
+    // --------
     public function __construct() {
         $this->db = (new Conexion())->conectar();
     }
